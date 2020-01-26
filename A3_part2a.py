850b0199dd4c411210911a366411540e99069327
nmm14 <nmm14@sfu.ca>
2020-01-26 13:32:30 -0800

    Dash-Updated example

--- A3_part2a.py
+++ A3_part2a.py
@@ -4,11 +4,10 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objects as go
-from dataprep.eda import plot_missing
 import os
 
 df = pd.DataFrame(columns = ['station', 'date', 'latitude', 'longitude', 'elevation', 'tmax'])
-path = '/home/arjun/Documents/SFU_Course_Work/Spring2020/cmpt733/A3/part2/tmax-2'
+path = '/home/jedixnavi/Downloads/Datasets/tmax-2'
 for filename in os.listdir(path):
     data = pd.read_csv(path+'/'+filename, compression='gzip', header = None, names = ['station', 'date', 'latitude', 'longitude', 'elevation', 'tmax'])
     df = df.append(data, ignore_index=True)
@@ -19,10 +18,19 @@ group_df = df.groupby(['year','month','station','latitude', 'longitude', 'elevat
 group_df = group_df[group_df['year']>1980.5]
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+def plot_1(selected_year):
+    filtered_df = group_df[group_df.year == selected_year]
+    fig = go.Figure(data=go.Scattergeo(lat = filtered_df['latitude'], lon = filtered_df['longitude'], text = filtered_df['tmax'], showlegend = True, marker = dict(size = 10, opacity = 0.8, reversescale = True, autocolorscale = False, symbol = 'square', cmin = -35, color = group_df['tmax'], cmax = 50, colorbar_title="Max Temp")))
+    fig.update_geos(resolution=50, showcoastlines=True, coastlinecolor="RebeccaPurple", showland=True, landcolor="ghostwhite", showocean=True, oceancolor="LightBlue", showlakes=True, lakecolor="Blue", showrivers=True, rivercolor="Blue")
+    fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
+    fig.update_layout(title='Global Maximum Temperature ', autosize=True, hovermode='closest', height=600, margin={"r":0,"t":0,"l":0,"b":0})
+
+    return fig
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
+    dcc.Graph(id='alien_map'),
     dcc.Slider(
         id='year-slider',
         min=group_df['year'].min(),
@@ -32,20 +40,11 @@ app.layout = html.Div([
         step=None
     )
 ])
-
-
 @app.callback(
-    Output('graph-with-slider', 'figure'),
+    Output('alien_map', 'figure'),
     [Input('year-slider', 'value')])
-
-def update_figure(selected_year):
-    filtered_df = group_df[group_df.year == selected_year]
-    fig = go.Figure(data=go.Scattergeo(lat = filtered_df['latitude'], lon = filtered_df['longitude'], text = filtered_df['tmax'], showlegend = True, marker = dict(size = 10, opacity = 0.8, reversescale = True, autocolorscale = False, symbol = 'square', colorscale = 'icefire_r', cmin = -35, color = group_df['tmax'], cmax = 50, colorbar_title="Max Temp")))
-    fig.update_geos(resolution=50, showcoastlines=True, coastlinecolor="RebeccaPurple", showland=True, landcolor="ghostwhite", showocean=True, oceancolor="LightBlue", showlakes=True, lakecolor="Blue", showrivers=True, rivercolor="Blue")
-    fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
-    fig.update_layout(title='Global Maximum Temperature ', autosize=True, hovermode='closest', height=600, margin={"r":0,"t":0,"l":0,"b":0})
-    fig.show()
-    #return(fig.show())
+def update_figure(X):
+    return plot_1(X)
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
